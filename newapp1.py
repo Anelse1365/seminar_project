504423f07834602277b778622519ce26823f964b
Anelse1365 <130572929+Anelse1365@users.noreply.github.com>
2024-06-24 01:52:51 +0700

    save

--- newapp1.py
+++ newapp1.py
@@ -4,6 +4,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from pymongo import MongoClient
 import re
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
@@ -37,9 +38,20 @@ def index():
             numbers = num_pattern.findall(question_text)
             
             for num_tag, number in numbers:
+                if "random" in number:
+                    params = number.split(',')
+                    min_val = int(params[1])
+                    max_val = int(params[2])
+                    if len(params) == 4 and params[3] == 'odd':
+                        number = random.choice([x for x in range(min_val, max_val + 1) if x % 2 != 0])
+                    else:
+                        number = random.randint(min_val, max_val)
+                else:
+                    number = int(number)
+                
                 # แทนที่ <numX> ด้วยค่าของ number
-                question_text = question_text.replace(f'<num{num_tag}>{number}</num{num_tag}>', number)
-                numbers_collection.insert_one({'number': int(number)})
+                question_text = question_text.replace(f'<num{num_tag}>{number}</num{num_tag}>', str(number))
+                numbers_collection.insert_one({'number': number})
 
             questions_collection.insert_one({'question': question_text})
 
